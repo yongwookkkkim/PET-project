f3d2515e62756091cd949deb716b4eb1034534dc
yongwookkkkim <99084710+yongwookkkkim@users.noreply.github.com>
2022-05-31 17:18:35 +0900

    label recognition list->numpy.array

--- pythoncodes/script.py
+++ pythoncodes/script.py
@@ -14,61 +14,61 @@ class im():
 #loading images
 for no in range(1,5):
     img=Image.open(f"icis{no}.jpg", 'r')
-    pix=img.load()
     width, height=img.size
+    pix=np.array(img,dtype='int16')
     icis.append(im(pix,width,height))
 
 for no in range(1,5):
     img=Image.open(f"kirkland{no}.jpg", 'r')
-    pix=img.load()
     width, height=img.size
+    pix=np.array(img,dtype='int16')
     kirkland.append(im(pix,width,height))
 
+#processing data for rgb difference graphs
+w0=np.arange(icis[3].width,dtype='int16')
+h0=np.arange(icis[3].height,dtype='int16')
+w,h=np.meshgrid(w0,h0)
+frmg=lambda width, height: icis[3].pix[height,width,0]-icis[3].pix[height,width,1]
+fgmb=lambda width, height: icis[3].pix[height,width,1]-icis[3].pix[height,width,2]
+fbmr=lambda width, height: icis[3].pix[height,width,2]-icis[3].pix[height,width,0]
+f0gmb=fgmb(w,h)
+f0bmr=fbmr(w,h)
+f0rmg=frmg(w,h)
+f0rmg=np.where(np.logical_and(f0rmg>-30, f0rmg<30),1,0)
+f0bmr=np.where(np.logical_and(f0bmr>-30, f0bmr<30),1,0)
+f0gmb=np.where(np.logical_and(f0gmb>-30, f0gmb<30),1,0)
+#f0rmg[np.logical_and(f0rmg>-30, f0rmg<30)]=100
+#f0rmg[f0rmg!=100]=0
 
-#plotting rgb difference graphs
-x=range(kirkland[1].width)
-y=range(kirkland[1].height)
-x0,y0=np.meshgrid(x,y)
-frmg=lambda a,b: kirkland[1].pix[a,b][0]-kirkland[1].pix[a,b][1]
-fgmb=lambda a,b: kirkland[1].pix[a,b][1]-kirkland[1].pix[a,b][2]
-fbmr=lambda a,b: kirkland[1].pix[a,b][2]-kirkland[1].pix[a,b][0]
-f0rmg=[]
-f0gmb=[]
-f0bmr=[]
-for height in y:
-    rowrmg=[]
-    rowgmb=[]
-    rowbmr=[]
-    for width in x:
-        if fgmb(width,height)<30 and fgmb(width,height)>-30:
-            rowgmb.append(100)
-        else:
-            rowgmb.append(0)
+#removing meaningless outliers / mutable
+#bottom
+f0bmr[0:280,:]=1
+f0gmb[0:280,:]=1
+f0rmg[0:280,:]=1
+#top
+f0bmr[1350:,:]=1
+f0gmb[1350:,:]=1
+f0rmg[1350:,:]=1
+#left
+f0bmr[:,0:200]=1
+f0gmb[:,0:200]=1
+f0rmg[:,0:200]=1
+#right
+f0bmr[:,1000:]=1
+f0gmb[:,1000:]=1
+f0rmg[:,1000:]=1
 
-        if fbmr(width,height)<30 and fbmr(width,height)>-30:
-            rowbmr.append(100)
-        else:
-            rowbmr.append(0)
-        if frmg(width,height)<30 and frmg(width,height)>-30:
-            rowrmg.append(100)
-        else:
-            rowrmg.append(0)
-        #rowgmb.append(fgmb(width,height))
-        #rowrmg.append(frmg(width,height))
-        #rowbmr.append(fbmr(width,height))
-    f0rmg.append(rowrmg)
-    f0bmr.append(rowbmr)
-    f0gmb.append(rowgmb)
+#plotting
 plt.subplot(1,3,1)
-plt.contour(x0,y0,f0rmg,[0])
+plt.contour(w,h,f0rmg,[0])
 plt.title("red minus green")
 
 plt.subplot(1,3,2)
-plt.contour(x0,y0,f0bmr,[0])
+plt.contour(w,h,f0bmr,[0])
 plt.title("blue minus red")
 
 plt.subplot(1,3,3)
-plt.contour(x0,y0,f0gmb,[0])
+plt.contour(w,h,f0gmb,[0])
 plt.title("green minus blue")
 
 plt.tight_layout()
